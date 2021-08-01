a0eda7459c9749efefc20c41cda4c49c495ff4f7
Gulzhan1994 <gulzhan0404@gmail.com>
2021-08-01 22:37:13 +0600

    First commit

--- js/script.js
+++ js/script.js
@@ -1,35 +1,42 @@
 $(document).ready(function(){
-    $('.main-slider').slick({
-      dots: false,
-      autoplay: false,
-      arrows: true,
-      appendArrows: $('.slider_blog .slider_arrow'),
-      pauseOnHover: false,
-      pauseOnFocus: false,
-      speed: 1500,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      spaceBetween: 30,
-      prevArrow: '<button type="button" class="slick_arrow slick_prev"></button>',
-      nextArrow: '<button type="button" class="slick_arrow slick_next"></button>',
-    
-    });
-});
-
-$(document).ready(function(){
-  $('.section-slider').slick({
+  $('.main-slider').slick({
     dots: false,
     autoplay: false,
     arrows: true,
-    appendArrows: $('.slider-blog .slider-arrow'),
+    appendArrows: $('.slider_blog .slider_arrow'),
     pauseOnHover: false,
     pauseOnFocus: false,
     speed: 1500,
     slidesToShow: 1,
     slidesToScroll: 1,
     spaceBetween: 30,
-    prevArrow: '<button type="button" class="slick-arrow slick-prev"></button>',
-    nextArrow: '<button type="button" class="slick-arrow slick-next"></button>',
-  
+    prevArrow: '<button type="button" class="slick_arrow slick_prev"></button>',
+    nextArrow: '<button type="button" class="slick_arrow slick_next"></button>',
   });
+});
+
+$(document).ready(function(){
+$('.section-slider').slick({
+  dots: false,
+  autoplay: false,
+  arrows: true,
+  appendArrows: $('.slider-blog .slider-arrow'),
+  pauseOnHover: false,
+  pauseOnFocus: false,
+  speed: 1500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  spaceBetween: 30,
+  prevArrow: '<button type="button" class="slick-arrow slick-prev"></button>',
+  nextArrow: '<button type="button" class="slick-arrow slick-next"></button>',
+
+});
+});
+
+let headerBurger = document.querySelector(".header_burger")
+let headerMenu = document.querySelector(".menu-list")
+
+headerBurger.addEventListener("click", function (event) {
+headerBurger.classList.toggle("header_burger__active");
+headerMenu.classList.toggle("menu-list__active");
 });

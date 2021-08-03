e2ed162db3587c18000a53063b4e4dcde93cc312
Gulzhan1994 <gulzhan0404@gmail.com>
2021-08-03 22:39:19 +0600

    First commit

--- js/script.js
+++ js/script.js
@@ -15,23 +15,29 @@ $(document).ready(function(){
   });
 });
 
-$(document).ready(function(){
-$('.section-slider').slick({
-  dots: false,
-  autoplay: false,
-  arrows: true,
-  appendArrows: $('.slider-blog .slider-arrow'),
-  pauseOnHover: false,
-  pauseOnFocus: false,
-  speed: 1500,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  spaceBetween: 30,
-  prevArrow: '<button type="button" class="slick-arrow slick-prev"></button>',
-  nextArrow: '<button type="button" class="slick-arrow slick-next"></button>',
+// $(document).ready(function(){
+// $('.section-slider').slick({
+//   dots: false,
+//   autoplay: false,
+//   arrows: true,
+//   appendArrows: $('.slider-blog .slider-arrow'),
+//   pauseOnHover: false,
+//   pauseOnFocus: false,
+//   speed: 1500,
+//   slidesToShow: 1,
+//   slidesToScroll: 1,
+//   spaceBetween: 30,
+//   prevArrow: '<button type="button" class="slick-arrow slick-prev"></button>',
+//   nextArrow: '<button type="button" class="slick-arrow slick-next"></button>',
 
-});
-});
+// });
+// });
+
+$(".section-content .section-list-header").click(function() {
+	$(".section-content .section-list-header").removeClass("active").eq($(this).index()).addClass("active");
+	$(".tower").hide().eq($(this).index()).fadeIn()
+}).eq(0).addClass("active");
+
 
 let headerBurger = document.querySelector(".header_burger")
 let headerMenu = document.querySelector(".menu-list")
